f73466035f9fa9e4db586e71f355cdc2194d77be
mvselegenenko <mvselegenenko@gmail.com>
2018-06-13 17:17:19 +0300

    Initial Commit

---
+++ Calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calc
+//
+//  Created by Михаил Селегененко on 13.06.2018.
+//  Copyright © 2018 Михаил Селегененко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
