e517f8e587f78f64fe17d38c5cbed47b2eadb5f8
mvselegenenko <mvselegenenko@gmail.com>
2018-06-13 18:50:21 +0300

    first commit this branch.

--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var display: UILabel!
+
+	@IBAction func buttonNumber(_ sender: Any) {
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
