01369b2e0421b3d673293cd5e9f8bad703a70bd5
mvselegenenko <mvselegenenko@gmail.com>
2018-06-16 06:24:00 +0300

    Create logic

---
+++ Calc/CalcBrain.swift
@@ -0,0 +1,14 @@
+//
+//  CalcBrain.swift
+//  Calc
+//
+//  Created by Михаил Селегененко on 13.06.2018.
+//  Copyright © 2018 Михаил Селегененко. All rights reserved.
+//
+
+import Foundation
+
+var stringValue: String = ""
+var firstOperand: Double = 0
+var secondOperand: Double = 0
+var result: Double = 0
--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -12,11 +12,49 @@ class ViewController: UIViewController {
 
 	@IBOutlet weak var display: UILabel!
 
-	@IBAction func buttonNumber(_ sender: Any) {
+	func printDisplay()  {
+		if stringValue == ""{
+			display.text = String(0)
+		} else {
+		display.text = stringValue
+		}
 	}
+
+	@IBAction func buttonNumber(_ sender: UIButton) {
+		stringValue += sender.currentTitle!
+		printDisplay()
+	}
+
+	@IBAction func buttonOperation(_ sender: UIButton) {
+		if stringValue != "" {
+		firstOperand = Double(stringValue)!
+			secondOperand = firstOperand
+			//Value has secondOperand, firstOperand is cleared
+			firstOperand = 0
+			stringValue = ""
+			printDisplay()
+			let stringOperator = sender.currentTitle!
+			switch stringOperator {
+			case "÷":
+				result = secondOperand / firstOperand
+			case "*":
+				result = secondOperand * firstOperand
+			case "+":
+				result = secondOperand + firstOperand
+			case "-":
+				result = secondOperand - firstOperand
+			default: break
+
+			}
+		}
+
+		print(firstOperand, secondOperand, result)
+	}
+
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		printDisplay()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
